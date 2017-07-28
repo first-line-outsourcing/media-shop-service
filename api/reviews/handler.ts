f73d1538b8f383c135401caf495175f5e0dbdae4
oskvortsov-bmt <oskvortsov@bestmood.tech>
2017-07-28 16:06:33 +0300

    edit according to PR comment

--- api/reviews/handler.ts
+++ api/reviews/handler.ts
@@ -3,15 +3,7 @@ import { Reviews } from './reviews';
 export function add(event, context, callback) {
   const reviews = new Reviews();
   reviews.add(event.body)
-    .then(() => {
-      callback(
-        null,
-        {
-          statusCode: 200,
-          body: "Success"
-        }
-      );
-    })
+    .then(() => callback(null, { message: "Success" }))
     .catch((error) => {
       console.log(error);
       return callback(error.statusCode ? `[${error.statusCode}] ${error.message}` : '[500] Server error. Please try later');
@@ -21,16 +13,7 @@ export function add(event, context, callback) {
 export function getByProductID(event, context, callback) {
   const reviews = new Reviews();
   reviews.getByProductID(event.path.productID)
-    .then((data) => {
-      console.log(data);
-      callback(
-        null,
-        {
-          statusCode: 200,
-          body: data.Items
-        }
-      );
-    })
+    .then((data) => callback(null, { result: data.Items }))
     .catch((error) => {
       console.log(error);
       return callback(error.statusCode ? `[${error.statusCode}] ${error.message}` : '[500] Server error. Please try later');
